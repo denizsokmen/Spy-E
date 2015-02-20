b048a06c7331887beb20c27b5904a681031c0588
denizsokmen <deniz.sokmen@ozu.edu.tr>
2015-02-20 12:08:07 +0200

    camera

---
+++ Agile/Camera.hpp
@@ -0,0 +1,26 @@
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera {
+    glm::quat orientation;
+    glm::mat4 projection;
+    glm::mat4 view;
+    glm::vec3 position;
+    glm::vec3 viewDirection;
+
+public:
+    Camera();
+    ~Camera();
+
+
+    void lookAt(glm::vec3 eye, glm::vec3 target, glm::vec3 up);
+    void perspective(float fov, float ratio, float near, float far) {
+        glm::perspective(fov, ratio, near, far);
+    }
+    void ortho(float left, float right, float top, float bot) {
+        glm::ortho(left, right, bot, top);
+    }
+
+
+};
