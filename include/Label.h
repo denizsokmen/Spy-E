3a8c3993eb969844dba5932134ba1715f6d32270
Taha Doğan Güneş <tdgunes@gmail.com>
2015-02-23 17:23:30 +0200

    Add abstract Label class

---
+++ include/Label.h
@@ -0,0 +1,34 @@
+//
+//  Label.h
+//  Spy-E
+//
+//  Created by Taha Doğan Güneş on 23/02/15.
+//  Copyright (c) 2015 Spy-E. All rights reserved.
+//
+
+
+#ifndef __SpyE__Label__
+#define __SpyE__Label__
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+
+#include "Renderable.h"
+#include "VertexBuffer.h"
+
+
+class Label: public Renderable {
+
+private:
+    float length;
+    std::string text;
+    VertexBuffer* vertexBuffer;
+
+public:
+    Label (glm::vec3 position, std::string text);
+
+};
+
+#endif /* defined(__SpyE__Label__) */
