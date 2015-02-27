e5d13ec93ada87134a72145b437b465ccb2c0eff
Enes <enessenel@outlook.com>
2015-02-27 17:43:35 +0200

    PhysicsWorld Added

---
+++ include/PhysicsWorld.h
@@ -0,0 +1,16 @@
+#ifndef PHYSICSWORLD_H_INC
+#define PHYSICSWORLD_H_INC
+
+#include "Body.h"
+
+class PhysicsWorld{
+
+    std::vector<Body> bodies;
+
+public:
+    PhysicsWorld();
+    ~PhysicsWorld();
+
+    Body createBody(glm::vec3 loc, glm::vec3 speed, glm::vec3 acc);
+};
+#endif
