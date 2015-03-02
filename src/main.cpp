fc50367805c8858f07d4ddf4dd9e3776784f4cf9
Taha Doğan Güneş <tdgunes@gmail.com>
2015-03-02 18:09:53 +0200

    Delete tinyxml2, add maps folder

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,21 @@
 //  Copyright (c) 2015 CS476. All rights reserved.
 //
 #include <stdbool.h>
+#include <iostream>
 #include "Game.h"
 
 Game* game;
 
 int main( void )
 {
+
     game = new Game();
     if (game->init(800, 600, "Spy-E", false)) {
         game->update();
     }
+
+
+
     game->end();
     delete game;
     return 0;
