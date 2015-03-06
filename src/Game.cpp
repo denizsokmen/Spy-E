325f4503dccb5116b4177e570bf5b439329acbd6
denizsokmen <deniz.sokmen@ozu.edu.tr>
2015-03-06 08:39:45 +0200

    Forward compatible flag for OpenGL 3.3+ on Mac OS

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <glfw3.h>
+#include <SDL_video.h>
 #include "SDL.h"
 #include "Timer.h"
 #include "Scene.h"
@@ -14,6 +15,8 @@
 
 bool Game::init(int width, int height, char const *title, bool fullScreen) {
     SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -24,6 +27,10 @@ bool Game::init(int width, int height, char const *title, bool fullScreen) {
     mainWindow = SDL_CreateWindow("Spy-E", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, ((fullScreen) ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_SHOWN) |SDL_WINDOW_OPENGL);
     mainGLContext = SDL_GL_CreateContext(mainWindow);
 
+    if (mainGLContext == 0) {
+        printf ("Error while creating OpenGL context: %s\n", SDL_GetError());
+        abort();
+    }
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
