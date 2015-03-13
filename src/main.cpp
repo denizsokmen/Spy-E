ffd44a69640a4f820efd2c42e6bb6c3e648c08e5
denizsokmen <sokmendeniz@gmail.com>
2015-03-13 21:52:45 +0200

    Dropped mingw support immediately.

--- src/main.cpp
+++ src/main.cpp
@@ -12,26 +12,7 @@
 
 Game* game;
 
-#if defined _WIN32 || defined __CYGWIN__
-#include <windows.h>
-int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
-{
 
-    game = new Game();
-    if (game->init(640, 480, "Spy-E", false)) {
-        TestGameSystem *test = new TestGameSystem(game);
-        game->getController()->addCoreSystem(test);
-        game->update();
-    }
-
-
-
-    game->end();
-    delete game;
-    return 0;
-
-}
-#else
 int main(int argc, char* argv[])
 {
     game = new Game();
@@ -47,4 +28,3 @@ int main(int argc, char* argv[])
     delete game;
     return 0;
 }
-#endif
