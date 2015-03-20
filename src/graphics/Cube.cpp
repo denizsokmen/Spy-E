b824c7336bd10317dd16afcc174f7d6636c79e9c
Taha Doğan Güneş <tdgunes@gmail.com>
2015-03-20 23:43:39 +0200

    Edit Cube.cpp for deleting debug print for checking color on every update

--- src/graphics/Cube.cpp
+++ src/graphics/Cube.cpp
@@ -11,7 +11,6 @@ Cube::Cube() {
 void Cube::render(Renderer *renderer) {
     Renderable::render(renderer);
     glUniform3fv(glGetUniformLocation(renderer->generalShader->id, "Color"), 1, &color[0]);
-    printf("Set color %f, %f, %f \n", color[0], color[1], color[2]);
 }
 
 
