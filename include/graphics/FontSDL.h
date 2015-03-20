dae082ade0e92d00984747d42fa0f61682ef7141
denizsokmen <sokmendeniz@gmail.com>
2015-03-20 23:08:35 +0200

    Font and font TODOs

--- include/graphics/FontSDL.h
+++ include/graphics/FontSDL.h
@@ -1,11 +1,19 @@
 #ifndef FONTSDL_H_INC
 #define FONTSDL_H_INC
+#include <vector>
+#include <glm/glm.hpp>
+
+class Texture;
 
 class FontSDL {
 
 public:
     FontSDL();
     ~FontSDL();
+	
+	std::vector<Texture*> charSet;
+	void loadFont(const char *fontname, int size);
+	void draw(glm::vec3 position, const wchar_t* text, ...);
 
 
 };
