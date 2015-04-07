4860ea29d59d3a55d4de9fb0222bfc2c11251375
denizsokmen <deniz.sokmen@ozu.edu.tr>
2015-04-07 13:08:57 +0300

    fixed shader bug, it looks much better now

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,6 +7,7 @@ layout(location = 1) in vec3 normal;
 uniform mat4 MVP;
 uniform mat3 NormalMatrix;
 uniform mat4 ModelMatrix;
+uniform mat4 ViewMatrix;
 uniform mat4 ModelViewMatrix;
 
 
@@ -20,6 +21,6 @@ void main() {
     gl_Position = MVP * vertexPosition_modelspace;
     normal_eyespace = NormalMatrix * normal;
     position_eyespace = ModelViewMatrix * vertexPosition_modelspace;
-    light_eyespace = ModelViewMatrix * vec4(10.0, 10.0,10.0, 1.0);
+    light_eyespace = ViewMatrix * vec4(10.0, 10.0,10.0, 1.0);
 
 }
